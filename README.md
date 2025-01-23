629b92845351ea44e46e975e1ec9a1996590c891
Amedeo Biolatti <amedeo.biolatti@gmail.com>
2025-01-23 17:32:27 +0100

    updated minimal_example.py and relative table in README.md

--- README.md
+++ README.md
@@ -19,20 +19,14 @@ fn(1)
 
 ### Mini-benchmark
 
-The mini-benchmark in `examples/minimal_example.py` returns the following times (in seconds)
+The mini-benchmark in `examples/minimal_example.py` returns the following times (in seconds on CPU)
 
-|                      | Compile + first run | 100 successive runs |
-|----------------------|---------------------|---------------------|
-| jax                  | 0.088991            | 1.660780            |
-| jax-jit              | 0.056053            | 0.049823            |
-| numpy                | 0.000353            | 0.025763            |
-| numba                | 16.327550           | 0.011057            |
-| numba-fast           | 16.965312           | 0.010609            |
-| jax:fori_loop        | 0.030407            | 2.830550            |
-| jax-jit:fori_loop    | 0.023105            | 0.014057            |
-| numpy:fori_loop      | 0.000431            | 0.031376            |
-| numba:fori_loop      | 0.132297            | 0.005540            |
-| numba-fast:fori_loop | 0.131045            | 0.005505            |
+|                     | Compile + first run | 100 runs             |
+|---------------------|---------------------|----------------------|
+| jax-jit             | 0.0393              | 0.1052 ($\pm$0.0029) |
+| numbax:numpize      | 0.0005              | 0.0379 ($\pm$0.0009) |
+| numbax:jit          | 0.2435              | 0.0116 ($\pm$0.0002) |
+| numbax:jit-fastmath | 0.1798              | 0.0116 ($\pm$0.0002) |
 
 # WARNING: Very Early Development 🚧
 
--- examples/minimal_example.py
+++ examples/minimal_example.py
@@ -1,72 +1,38 @@
 import gc
 
 import jax
-import numba
 import time
 import numpy as np
 
 import numbax
 from numbax import numpize
 
-N = 1000
+N = 100
 
 
 def fn(x):
-    # def step(carry, x):
-    #     y = carry + x
-    #     return carry, y
-    #
-    # stacked, x = jax.lax.scan(step, x, xs=jax.numpy.arange(N))
-
-    for _ in range(N):
-        x = jax.numpy.cos(x)
-        x = jax.numpy.sin(x)
-
-    x = x * 2 + 1
-    return x
-
-
-def fn_loop(x):
-    # def step(carry, x):
-    #     y = carry + x
-    #     return carry, y
-    #
-    # stacked, x = jax.lax.scan(step, x, xs=jax.numpy.arange(N))
-
     def step(i, x):
-        x = jax.lax.cond(i >= 10, lambda: jax.numpy.sin(x), lambda: jax.numpy.cos(x))
-        x = jax.numpy.cos(x)
-        x = jax.numpy.sin(x)
+        x = jax.numpy.cos(x + i)
+        x = jax.numpy.sin(x + i)
         return x
 
     x = jax.lax.fori_loop(0, N, step, x)
-
     x = x * 2 + 1
     return x
 
 
 jaxpr = jax.make_jaxpr(fn)(1.)
-fn_numpized = numpize.numpize_jaxpr(jaxpr)
-
-jaxpr = jax.make_jaxpr(fn_loop)(1.)
 fn_loop_numpized = numpize.numpize_jaxpr(jaxpr)
 
-print("| | Compile + first run | 100 successive runs |")
+print("| | Compile + first run | 100 runs |")
 print("|--|--|--|")
 for (f_, f_name) in [
-    # (fn, 'jax'),
-    # (jax.jit(fn), 'jax-jit'),
-    # (fn_numpized, 'numpy'),
-    # (numbax.jit(fn), 'numba'),
-    # (numbax.jit(fn, fast=True), 'numba-fast'),
-    # (fn_loop, 'jax:fori_loop'),
-    (jax.jit(fn_loop), 'jax-jit:fori_loop'),
-    (fn_loop_numpized, 'numpy:fori_loop'),
-    (numbax.jit(fn_loop), 'numba:fori_loop'),
-    (numbax.jit(fn_loop, fast=True), 'numba-fast:fori_loop'),
+    (jax.jit(fn), 'jax-jit'),
+    (fn_loop_numpized, 'numbax:numpize'),
+    (numbax.jit(fn), 'numbax:jit'),
+    (numbax.jit(fn, fast=True), 'numbax:jit-fastmath'),
 ]:
-    # X = np.zeros((100,), dtype=np.float32)
-    X = np.float32(0.5)
+    X = np.zeros((1024,), dtype=np.float32)
     if "jax" in f_name:
         X = jax.device_put(X)
 
@@ -76,7 +42,7 @@ for (f_, f_name) in [
         t1 = time.perf_counter()
 
         times = []
-        for _ in range(20):
+        for _ in range(100):
             t2 = time.perf_counter()
             for _ in range(100):
                 out = f_(X)
@@ -84,10 +50,10 @@ for (f_, f_name) in [
                     out.block_until_ready()
             t3 = time.perf_counter()
             times.append(t3 - t2)
-            gc.collect()
-            time.sleep(0.0001)
+        time.sleep(0.001)
 
-        print("| %20s  |  %.6f  |  %.6f (+-%.6f)  |" % (f_name, t1 - t0, 1000 * np.mean(times), 2000 * np.std(times)))
+        times = np.array(times)
+        print("| %20s  |  %.4f  |  %.4f (+-%.4f)  |" % (f_name, t1 - t0, np.mean(times), np.std(times)))
         time.sleep(0.1)
     except Exception as e:
         print(e)
